3dbf4d7d3a1794b2e83177726e618e034379fb58
Jim Saari <jim.saari97@gmail.com>
2020-07-06 18:12:52 +0300

    add item configurations

--- dataset/src/config.js
+++ dataset/src/config.js
@@ -7,6 +7,50 @@ module.exports = [
     name: "ssd",
     product: "internal-hard-drive",
     query: "t=0",
+  },
+  {
+    name: "motherboard",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "power-supply",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "memory",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "sound-card",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "internal-hard-drive",
+    query: "t=1,5200,5400,5700,5760,5900,7200,10000,10025,10500,10520,15000",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "video-card",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "cpu-cooler",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "case",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "optical-drive",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "wired-network-card",
+    shape: ["td:nth-child(2) a p"],
+  },
+  {
+    name: "wireless-network-card",
     shape: ["td:nth-child(2) a p"],
   },
 ];
