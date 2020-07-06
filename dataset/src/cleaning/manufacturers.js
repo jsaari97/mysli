e11c3db161f08f9f3653646f38ae57bb99ec79a6
Jim Saari <jim.saari97@gmail.com>
2020-07-06 19:34:42 +0300

    fetch manufacturer data

---
+++ dataset/src/cleaning/manufacturers.js
@@ -0,0 +1,43 @@
+const fs = require("fs").promises;
+const path = require("path");
+const xray = require("x-ray");
+
+const x = xray();
+
+const outputDir = path.join(__dirname, "/data");
+
+module.exports = async () => {
+  try {
+    let data = await x(
+      "https://en.wikipedia.org/wiki/List_of_computer_hardware_manufacturers",
+      ".mw-parser-output",
+      ["div.div-col > ul > li > a"]
+    );
+
+    data = data
+      .map((item) =>
+        item
+          .replace(
+            /(\(.*?\)|Technology|International|Memory|Technologies|Electronics|Corporation|Interactive|,|Inc\.|Components|^G-)/gi,
+            ""
+          )
+          .trim()
+      )
+      .filter(Boolean);
+
+    // Remove duplicates
+    data = [...new Set(data)];
+
+    await fs.mkdir(outputDir, { recursive: true });
+
+    await fs.writeFile(
+      path.join(outputDir, "manufacturers.json"),
+      JSON.stringify(data, "", 2),
+      "utf8"
+    );
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+module.exports();
